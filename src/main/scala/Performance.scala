b7d66b924a7a9cbab4133a925bc60689f71de9f4
George K. Thiruvathukal <gkt@cs.luc.edu>
2015-02-14 10:34:25 -0600

    use command-line arguments to vary size and reps
    Signed-off-by: George K. Thiruvathukal <gkt@cs.luc.edu>

--- src/main/scala/Performance.scala
+++ src/main/scala/Performance.scala
@@ -1,9 +1,12 @@
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 import scala.util.Random.nextInt
+import scala.util.Try
 
 object Performance {
 
+  // default experiment sizes
   val REPS = 100000
+  val SIZE = 5000
 
   // TODO modify this program so that it runs for SIZE = 10, 100, 1000, 10000
   // and for both ArrayBuffer and ListBuffer
@@ -13,24 +16,29 @@ object Performance {
 
 
   def main(args: Array[String]): Unit = {
+ 
+    val arg0 = Try(args(0).toInt).toOption
+    val reps = arg0.getOrElse(REPS)
+
+    val arg1 = Try(args(1).toInt).toOption
+    val size = arg1.getOrElse(SIZE)
 
     val fixture = new ArrayBuffer[Int]
-    val size = 10000
 
-    timeThis(s"${fixture.getClass.getSimpleName} fixture creation") { 
+    timeThis(s"${fixture.getClass.getSimpleName} fixture creation (size = $size)") { 
       for (i <- 0 until size) fixture += i
     }
 
-    timeThis(s"${fixture.getClass.getSimpleName} fixture size = ${fixture.length} random access") {
+    timeThis(s"${fixture.getClass.getSimpleName} reps = $reps fixture size = ${fixture.length} random access") {
       var x = 0L
-      for (r <- 0 until REPS) {
+      for (r <- 0 until reps) {
         val randomPosition = nextInt(fixture.length)
         x = fixture(randomPosition % size)
       }
     }
 
-    timeThis(s"${fixture.getClass.getSimpleName} fixture size = ${fixture.length} random add/remove") {
-      for (r <- 0 until REPS) {
+    timeThis(s"${fixture.getClass.getSimpleName} reps = $reps fixture size = ${fixture.length} random add/remove") {
+      for (r <- 0 until reps) {
         val randomPosition = nextInt(fixture.length)
         fixture.insert(randomPosition, 77)
         fixture.remove(randomPosition)
@@ -42,7 +50,7 @@ object Performance {
     val time0 = System.currentTimeMillis
     val b = block
     val time1 = System.currentTimeMillis - time0
-    println("Timing " + s + " = " + time1)
+    println(s"$s = $time1 ms")
     b
   }
 }
